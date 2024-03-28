93b182e6d78ad11c5780bd8d15f9d08d7eb24471
mmmim24 <mustaq24@student.sust.edu>
2024-03-28 12:38:28 +0600

    users

--- frontend/Sexual_Harrasment_Prevention_Cell/src/contexts/StepperContext.jsx
+++ frontend/Sexual_Harrasment_Prevention_Cell/src/contexts/StepperContext.jsx
@@ -1,20 +1,21 @@
 import { createContext, useContext, useState } from "react";
 
-const StepperContext = createContext({ userData: "", setUserData: null,submit:false,setSubmit:()=>{} });
+const StepperContext = createContext({ userData: "", setUserData: null,submit:false,setSubmit:()=>{} ,users:{},setUsers:()=>{} });
 
 export function UseContextProvider({ children }) {
   const [userData, setUserData] = useState("");
+  const [users,setUsers] = useState({});
   const [submit, setSubmit] = useState(false);
 
   return (
-    <StepperContext.Provider value={{ userData, setUserData,submit, setSubmit }}>
+    <StepperContext.Provider value={{ userData, setUserData,submit, setSubmit,users,setUsers }}>
       {children}
     </StepperContext.Provider>
   );
 }
 
 export function useStepperContext() {
-  const { userData, setUserData,submit,setSubmit   } = useContext(StepperContext);
+  const { userData, setUserData,submit,setSubmit,users,setUsers   } = useContext(StepperContext);
 
-  return { userData, setUserData,submit,setSubmit };
+  return { userData, setUserData,submit,setSubmit,users,setUsers };
 }
