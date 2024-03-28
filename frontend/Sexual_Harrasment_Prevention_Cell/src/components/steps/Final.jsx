e672c3fef06390871735666bb742aaf450937aea
mmmim24 <mustaq24@student.sust.edu>
2024-03-28 12:44:13 +0600

    copy ID

--- frontend/Sexual_Harrasment_Prevention_Cell/src/components/steps/Final.jsx
+++ frontend/Sexual_Harrasment_Prevention_Cell/src/components/steps/Final.jsx
@@ -7,19 +7,19 @@ import {useNavigate} from 'react-router-dom';
 var submit = false;
 
 export default function Final() {
-  const {userData, setUserData} = useStepperContext();
+  const {userData, setUserData,users} = useStepperContext();
   const [complaint,setComplaint] = React.useState({});
   const navigate = useNavigate();
   const Preview = (e) =>{
     e.preventDefault();
     if(userData){
       if(!submit){
-        axios.post('https://cse350-backend-production.up.railway.app/complaint/add',userData)
+        axios.post('https://cse350-backend-production.up.railway.app/complaint/add',{userData,users})
             .then(res=>{
+                // console.log(res.data)
                 setComplaint(res.data);
                 submit = true;
-              } 
-              )
+              })
               .catch(err=>console.log(err));
         }
             // console.log(complaint);
@@ -33,9 +33,14 @@ export default function Final() {
     e.preventDefault();
     navigate('/dashboard')
     window.location.reload();
-}
+  }
+  const copy = (e) =>{
+    e.preventDefault();
+    navigator.clipboard.writeText(complaint.ID);
+    alert('ID copied to clipboard');
+  }
     return (
-      <div className="m-10">
+      <div className="m-6">
         <div className="flex flex-col items-center">
           <div className="wrapper">
             <svg
@@ -62,8 +67,14 @@ export default function Final() {
             appreciation for Being brave !
           </div>
           <div className="text-lg font-semibold text-gray-500">
-            Your Complaint has been filed. <p>{complaint.ID}</p>
+            Your Complaint has been filed. 
           </div>
+          {submit&&
+          <div className="flex gap-2 my-10 items-center">
+            <p className="text-[#D70000]">{complaint.ID}</p>
+            <button className="px-4 py-2 bg-[#136F63] text-white border rounded-lg focus:shadow-outline hover:bg-[#FAFFF3] hover:text-[#136F63] hover:border-[#136F63]" onClick={copy}>Copy this ID</button>
+          </div>
+          }
           <div>
             <button onClick={Preview} className="px-4 py-2 bg-[#136F63] text-white border rounded-lg focus:shadow-outline hover:bg-[#FAFFF3] hover:text-[#136F63] hover:border-[#136F63] mr-9">
               Preview
--- frontend/Sexual_Harrasment_Prevention_Cell/src/components/steps/Info.jsx
+++ frontend/Sexual_Harrasment_Prevention_Cell/src/components/steps/Info.jsx
@@ -1,14 +1,26 @@
+import React from "react";
 import { useStepperContext } from "../../contexts/StepperContext";
+import axios from "axios";
 
 export default function Info() {
-  const { userData, setUserData ,submit,setSubmit} = useStepperContext();
-
+  const { userData, setUserData ,submit,setSubmit,users,setUsers} = useStepperContext();
   const handleChange = (e) => {
     const { name, value } = e.target;
     // setSubmit(true);
     setUserData({ ...userData, [name]: value });
   };
-
+  React.useEffect(()=>{
+    axios.post('https://cse350-backend-production.up.railway.app/user/dashboard')
+        .then(res=>{
+            if(res.data.valid===true){
+                setUsers(res.data);
+            }
+            else{
+                navigate('/');
+            }
+        })
+        .catch(err=>console.log(err));
+  },[]);
   return (
     <div className="flex flex-col ">
       <div className="mx-2 w-full flex-1">
