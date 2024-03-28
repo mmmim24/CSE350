ccbebcf09f6ad98858f87136ec7835d8ee98d5f0
mmmim24 <mustaq24@student.sust.edu>
2024-03-28 21:43:21 +0600

    change password

--- frontend/Sexual_Harrasment_Prevention_Cell/src/components/Profile.jsx
+++ frontend/Sexual_Harrasment_Prevention_Cell/src/components/Profile.jsx
@@ -5,6 +5,34 @@ import { useNavigate } from 'react-router-dom';
 const Profile = () => {
     const [user,setUser] = React.useState({});
     const navigate = useNavigate();
+    const changePassword = (e) =>{
+        e.preventDefault();
+        const {email} = user;
+        const password = document.getElementById('password').value;
+        const newPassword = document.getElementById('newPassword').value;
+        const confirmPassword = document.getElementById('confirmPassword').value;
+        console.log(password,newPassword,confirmPassword);
+        console.log(email)
+        if(newPassword===confirmPassword){
+            axios.post('https://cse350-backend-production.up.railway.app/user/changePassword',{email,password,newPassword})
+                .then(res=>{
+                    console.log(res.data);
+                    if(res.data==="The password is incorrect"){
+                        alert('Wrong Password');
+                    }
+                    else{
+                        alert('Password updated successfully');
+                        setTimeout(() => {
+                            navigate('/');
+                        }, 500);
+                    }
+                })
+                .catch(err=>console.log(err));
+        }
+        else{
+            alert('Passwords do not match');
+        }
+    }
     React.useEffect(()=>{
         axios.post('https://cse350-backend-production.up.railway.app/user/dashboard')
             .then(res=>{
@@ -29,6 +57,20 @@ const Profile = () => {
                 <div className='flex gap-4'>
                     <h1 className='text-2xl'>Email:</h1>
                     <h1 className='text-2xl'>{user.email}</h1>
+                </div>
+                <div className=''>
+                    <h1 className='text-2xl my-5'>Change Password</h1>
+                    <form action='post' onSubmit={changePassword}>
+                        <label className='m-2'>Enter old password</label>
+                        <input type='password' id='password' placeholder='Enter old password' className='w-full appearance-none p-1 px-2 text-gray-800 outline-none rounded-md border-2 border-gray-400 bg-gray-200' required/><br/>
+                        <label className='m-2'>Enter new password</label>
+                        <input type='password' id='newPassword' placeholder='Enter new password' className='w-full appearance-none p-1 px-2 text-gray-800 outline-none rounded-md border-2 border-gray-400 bg-gray-200' required/><br/>
+                        <label className='m-2'>Confirm new password</label>
+                        <input type='password' id='confirmPassword' placeholder='Confirm new password' className='w-full appearance-none p-1 px-2 text-gray-800 outline-none rounded-md border-2 border-gray-400 bg-gray-200' required/><br/>
+                        <button className='bg-[#136F63] text-white hover:bg-[#FAFFF3] hover:border-[1px] hover:border-[#136F63] hover:text-[#136F63] hover:py-[5px] text-xl py-1.5 px-3.5 rounded-lg mt-5' type='submit'>
+                            Change Password
+                        </button>
+                    </form>
                 </div>
             </div>
         </main>
