6989b375995d6d19cd9d849b3a8d13ae265999ab
mmmim24 <mustaq24@student.sust.edu>
2024-04-20 14:00:50 +0600

    filter complaints

--- frontend/Sexual_Harrasment_Prevention_Cell/src/components/Login.jsx
+++ frontend/Sexual_Harrasment_Prevention_Cell/src/components/Login.jsx
@@ -47,21 +47,25 @@ function Login() {
                 <form action='post' onSubmit={handleSubmit}>
                     <div className='flex justify-between m-3 p-3'>
                         <label className='py-1' htmlFor='email'>Email</label>
-                        <input  className=' text-center rounded-md bg-[#FBFFF6]'
-                                name='email' 
-                                type='email' 
-                                placeholder='Enter Email' 
-                                onChange={(e)=>{setEmail(e.target.value)}} 
-                                required/> 
+                        <input 
+                            className='w-[70%] text-center rounded-md bg-[#FBFFF6]'
+                            name='email' 
+                            type='email' 
+                            placeholder='Enter Email' 
+                            onChange={(e)=>{setEmail(e.target.value)}} 
+                            required
+                        /> 
                     </div>
                     <div className='flex justify-between m-3 p-3'>
                         <label className='py-1' htmlFor='password'>Password</label>
-                        <input  className=' text-center rounded-md bg-[#FBFFF6]'
-                                name='password' 
-                                type='password' 
-                                placeholder='Enter Password' 
-                                onChange={(e)=>{setPassword(e.target.value)}}
-                                required/> 
+                        <input  
+                            className='w-[70%] text-center rounded-md bg-[#FBFFF6]'
+                            name='password' 
+                            type='password' 
+                            placeholder='Enter Password' 
+                            onChange={(e)=>{setPassword(e.target.value)}}
+                            required
+                        /> 
                     </div>
                     <button className='bg-[#136F63] text-white hover:bg-[#FAFFF3] hover:border-[1px] hover:border-[#136F63] hover:text-[#136F63] hover:py-[7px] text-xl py-2 px-4 rounded-lg' type='submit'>Login</button>
                     <p className='text-[#D70000] mt-4'>Please Contact The Registrar Building if you have no account</p>
--- frontend/Sexual_Harrasment_Prevention_Cell/src/components/ViewComplaints.jsx
+++ frontend/Sexual_Harrasment_Prevention_Cell/src/components/ViewComplaints.jsx
@@ -4,11 +4,15 @@ import {useNavigate} from 'react-router-dom';
 
 const ViewComplaints = () => {
     const [complaints,setComplaints] = React.useState([]);
+    const [filteredComplaints,setFilteredComplaints] = React.useState([]);
     const navigate = useNavigate();
 
     const ViewComplaint = (ID) =>{
         navigate(`/complaint/${ID}`);
-    }    
+    }
+    const FilterComplaint = (status) =>{
+        setFilteredComplaints(complaints.filter(complaint=>complaint.status===status));
+    }  
     React.useEffect(()=>{
         axios.get('https://cse350-backend-production.up.railway.app/complaint/getAll')
             .then(res=>{
@@ -20,16 +24,38 @@ const ViewComplaints = () => {
   return (
     <React.Fragment>
         <main>
-            <h1 className='text-3xl font-bold py-[50px]'>Complaints</h1>
+            <h1 className='text-3xl font-bold py-[16px]'>Complaints</h1>
+            <div className='py-[32px] font-semibold grid grid-cols-3 place-items-center'>
+                
+                <button className='p-2 hover:p-[7px] w-[128px] bg-[#136F63] hover:bg-[#FAFFF3] focus:bg-[#D70000] text-slate-100 rounded-lg hover:border-[1px] hover:border-[#136F63] hover:text-[#136F63]' to='/' onClick={(e)=>FilterComplaint('In Progress')}>In Progress</button>
+                <button className='p-2 hover:p-[7px] w-[128px] bg-[#136F63] hover:bg-[#FAFFF3] focus:bg-[#D70000] text-slate-100 rounded-lg hover:border-[1px] hover:border-[#136F63] hover:text-[#136F63]' to='/' onClick={(e)=>FilterComplaint('Processing')}>Processing</button>
+                <button className='p-2 hover:p-[7px] w-[128px] bg-[#136F63] hover:bg-[#FAFFF3] focus:bg-[#D70000] text-slate-100 rounded-lg hover:border-[1px] hover:border-[#136F63] hover:text-[#136F63]' to='/' onClick={(e)=>FilterComplaint('Resolved')}>Resolved</button>
+            </div>
+            {filteredComplaints.length===0 && 
+                <div className='grid grid-cols-3'>
+                    {complaints.map(complaint=>{
+                        return (
+                            <div className='p-5 m-5 justify text-justify rounded-lg bg-[#FAFFF3] border-2 border-[#136F63] w-[320px] h-[320px]' key={complaint.ID}>
+                                <h2 className='font-bold'>Submitted by : {complaint.fullName}</h2>
+                                <p className='font-semibold'>Time : {complaint.date}</p>
+                                <p className='font-semibold'>Status : {complaint.status}</p>
+                                <div className='my-[128px] grid'>
+                                    <button className='p-2 font-semibold bg-[#136F63] hover:bg-[#FAFFF3] text-slate-100 rounded-lg hover:border-[1px] hover:border-[#136F63] hover:text-[#136F63]' to='/' onClick={(e)=>ViewComplaint(complaint.ID)}>Details</button>
+                                </div>
+                            </div>
+                        )})
+                    }
+                </div>
+            }
             <div className='grid grid-cols-3'>
-                {complaints.map(complaint=>{
+                {filteredComplaints.map(complaint=>{
                     return (
                         <div className='p-5 m-5 justify text-justify rounded-lg bg-[#FAFFF3] border-2 border-[#136F63] w-[320px] h-[320px]' key={complaint.ID}>
                             <h2 className='font-bold'>Submitted by : {complaint.fullName}</h2>
                             <p className='font-semibold'>Time : {complaint.date}</p>
                             <p className='font-semibold'>Status : {complaint.status}</p>
                             <div className='my-[128px] grid'>
-                                <button className='p-2 bg-[#136F63] hover:bg-[#FAFFF3] text-slate-100 rounded-lg hover:border-[1px] hover:border-[#136F63] hover:text-[#136F63]' to='/' onClick={(e)=>ViewComplaint(complaint.ID)}>Details</button>
+                                <button className='p-2 font-semibold bg-[#136F63] hover:bg-[#FAFFF3] text-slate-100 rounded-lg hover:border-[1px] hover:border-[#136F63] hover:text-[#136F63]' to='/' onClick={(e)=>ViewComplaint(complaint.ID)}>Details</button>
                             </div>
                         </div>
                     )})
